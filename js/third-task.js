501160a97382181e7e0fed4e650dfaa9a97b34b0
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-06-29 04:45:53 +0300

    HW 11

---
+++ js/third-task.js
@@ -0,0 +1,98 @@
+/**
+ * Задание 3
+
+Перепиши функцию makeTransaction() так, чтобы она не использовала 
+callback-функции onSuccess и onError, а принимала всего один параметр 
+transaction и возвращала промис.
+
+
+p.s. По promisification сложности обычно вызывает третье 
+задание: обратите внимание, что функции resolve/reject может принять 
+только один аргумент. Отсюда следует, что необходимо, среди прочего, 
+подкорректировать параметры функции logSuccess таким образом, 
+чтобы она принимала либо объект, либо массив.
+ */
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+/* --------------------------------------------------------------------------------- */
+// const makeTransaction = (transaction, onSuccess, onError) => {
+//   const delay = randomIntegerFromInterval(200, 500);
+
+//   setTimeout(() => {
+//     const canProcess = Math.random() > 0.3;
+
+//     if (canProcess) {
+//       onSuccess(transaction.id, delay);
+//     } else {
+//       onError(transaction.id);
+//     }
+//   }, delay);
+// };
+
+// const logSuccess = (id, time) => {
+//   console.log(`Transaction ${id} processed in ${time}ms`);
+// };
+
+// const logError = id => {
+//   console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+
+/*
+ * Работает так
+ */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+
+
+
+
+/* --------------------------------------------------------------------------------- */
+const makeTransaction = (transaction) => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      } 
+        reject(transaction.id);
+      
+    }, delay);
+  })
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
